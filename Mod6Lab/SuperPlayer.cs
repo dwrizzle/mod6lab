040157012c7e07c8cb53d5e7fb3a4302d0f630ed
Desiree Wright <xv7406ml@go.minneapolis.edu>
2024-10-15 08:01:16 +0000

    initial commit

---
+++ Mod6Lab/SuperPlayer.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Mod6Lab
+{
+    internal class SuperPlayer : Player
+    {
+        //Additonal properties or methods specific to SuperPlayer
+        //For example:
+        public void Fly()
+        {
+            Console.WriteLine("SuperPlayer is flying!");
+        }
+    }
+}
